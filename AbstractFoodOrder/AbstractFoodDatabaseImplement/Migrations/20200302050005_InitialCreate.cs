010584bb0208f730c185f8820dde286f1dc20e56
tim.chuppuev@mail.ru <tim.chuppuev@mail.ru>
2020-03-16 17:55:40 +0400

    Лабораторная работа 3

---
+++ AbstractFoodOrder/AbstractFoodDatabaseImplement/Migrations/20200302050005_InitialCreate.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AbstractFoodDatabaseImplement.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Foods",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ComponentName = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Foods", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Kits",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    KitName = table.Column<string>(nullable: true),
+                    Price = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Kits", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Count = table.Column<int>(nullable: false),
+                    Sum = table.Column<decimal>(nullable: false),
+                    Status = table.Column<int>(nullable: false),
+                    DateCreate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "KitComponents",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    KitId = table.Column<int>(nullable: false),
+                    FoodId = table.Column<int>(nullable: false),
+                    Count = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_KitComponents", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_KitComponents_Foods_FoodId",
+                        column: x => x.FoodId,
+                        principalTable: "Foods",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_KitComponents_Kits_KitId",
+                        column: x => x.KitId,
+                        principalTable: "Kits",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_KitComponents_FoodId",
+                table: "KitComponents",
+                column: "FoodId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_KitComponents_KitId",
+                table: "KitComponents",
+                column: "KitId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "KitComponents");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Foods");
+
+            migrationBuilder.DropTable(
+                name: "Kits");
+        }
+    }
+}
---
+++ AbstractFoodOrder/AbstractFoodDatabaseImplement/Models/KitFood.cs
@@ -0,0 +1,19 @@
+﻿using AbstractFoodFileImplement.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace AbstractFoodDatabaseImplement.Models
+{
+    public class KitFood
+    {
+        public int Id { get; set; }
+        public int KitId { get; set; }
+        public int FoodId { get; set; }
+        [Required]
+        public int Count { get; set; }
+        public virtual Food Food { get; set; }
+        public virtual Kit Kit { get; set; }
+    }
+}
