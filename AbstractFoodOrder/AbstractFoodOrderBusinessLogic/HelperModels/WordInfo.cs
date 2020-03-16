31add69c21356c11d1f6a3a8c3268b18baa9bce2
tim.chuppuev@mail.ru <tim.chuppuev@mail.ru>
2020-03-16 19:10:30 +0400

    Лабораторная работа 4(не доделанная)

---
+++ AbstractFoodOrder/AbstractFoodOrderBusinessLogic/HelperModels/WordInfo.cs
@@ -0,0 +1,14 @@
+﻿using AbstractFoodOrderBusinessLogic.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractFoodOrderBusinessLogic.HelperModels
+{
+    class WordInfo
+    {
+        public string FileName { get; set; }
+        public string Title { get; set; }
+        public List<KitViewModel> Foods { get; set; }
+    }
+}
---
+++ AbstractFoodOrder/AbstractFoodOrderBusinessLogic/ViewModels/ReportKitFoodViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractFoodOrderBusinessLogic.ViewModels
+{
+    public class ReportKitFoodViewModel
+    {
+        public string FoodName { get; set; }
+        public int TotalCount { get; set; }
+        public List<Tuple<string, int>> Kits { get; set; }
+    }
+}
