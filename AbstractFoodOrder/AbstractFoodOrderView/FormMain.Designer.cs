cee93aa6d549e6b6fbb7e7be66cc5e9d6de66a65
ChuppuevT <43821694+ChuppuevT@users.noreply.github.com>
2020-09-09 21:43:08 +0400

    Update FormMain.Designer.cs

--- AbstractFoodOrder/AbstractFoodOrderView/FormMain.Designer.cs
+++ AbstractFoodOrder/AbstractFoodOrderView/FormMain.Designer.cs
@@ -216,7 +216,6 @@
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
-
         }
 
         #endregion
